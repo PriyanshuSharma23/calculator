b23e63c75272b1b741d55fd0bae188acee8ae33a
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2021-11-23 12:33:14 +0530

    version 1 final

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,147 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ButtonSection from "./buttonSection";
 
 function App() {
+  const [current, setCurrent] = useState("0");
+  const [prevNum, setPrevNum] = useState<string>("");
+
+  const operate = (a: number, b: number, operation: string) => {
+    switch (operation) {
+      case "+":
+        console.log(operation);
+        return a + b;
+
+      case "-":
+        return a - b;
+
+      case "*":
+        return a * b;
+
+      case "/":
+        return a / b;
+
+      default:
+        return a;
+    }
+  };
+
+  const plus = () => {
+    if (!prevNum) {
+      setPrevNum(current + " +");
+      setCurrent("0");
+    } else {
+      const operation = prevNum.slice(prevNum.length - 1);
+      const a = parseFloat(prevNum.slice(0, prevNum.length - 1));
+      const b = parseFloat(current);
+      const result = operate(a, b, operation);
+      setPrevNum(result + " +");
+      setCurrent("0");
+    }
+  };
+
+  const minus = () => {
+    if (!prevNum) {
+      setPrevNum(current + " -");
+      setCurrent("0");
+    } else {
+      const operation = prevNum.slice(prevNum.length - 1);
+      const a = parseFloat(prevNum.slice(0, prevNum.length - 1));
+      const b = parseFloat(current);
+      const result = operate(a, b, operation);
+      setPrevNum(result + " -");
+      setCurrent("0");
+    }
+  };
+
+  const multiply = () => {
+    if (!prevNum) {
+      setPrevNum(current + " *");
+      setCurrent("0");
+    } else {
+      const operation = prevNum.slice(prevNum.length - 1);
+      const a = parseFloat(prevNum.slice(0, prevNum.length - 1));
+      const b = parseFloat(current);
+      const result = operate(a, b, operation);
+      setPrevNum(result + " *");
+      setCurrent("0");
+    }
+  };
+
+  const divide = () => {
+    if (!prevNum) {
+      setPrevNum(current + " /");
+      setCurrent("0");
+    } else {
+      const operation = prevNum.slice(prevNum.length - 1);
+      const a = parseFloat(prevNum.slice(0, prevNum.length - 1));
+      const b = parseFloat(current);
+      const result = operate(a, b, operation);
+      setPrevNum(result + " /");
+      setCurrent("0");
+    }
+  };
+  const inputAdd = (num: string) => {
+    if (num === ".") {
+      if (!current.includes(".")) setCurrent((prev) => prev + num);
+      return;
+    }
+
+    if (current === "0") {
+      setCurrent(num);
+      return;
+    }
+    setCurrent((prev) => prev + num);
+  };
+
+  const clear = () => {
+    setCurrent("0");
+    setPrevNum("");
+  };
+
+  const del = () => {
+    if (current !== "") {
+      const newString = current.slice(0, current.length - 1);
+      if (newString) {
+        setCurrent(newString);
+        return;
+      }
+      setCurrent("0");
+      return;
+    }
+  };
+
+  const equals = () => {
+    if (prevNum) {
+      const operation = prevNum.slice(prevNum.length - 1);
+      const a = parseFloat(prevNum);
+      const b = parseFloat(current);
+
+      const result = operate(a, b, operation);
+      setCurrent(result.toString());
+      setPrevNum("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-1/3 mx-auto mt-10 overflow-x-hidden">
+      {/* Calculator Screen */}
+      <div data-previousval className="text-right text-gray-700 italic block h-5">
+        {prevNum}
+      </div>
+      <div className="text-4xl font-semibold text-right">{current}</div>
+
+      {/* Button Section */}
+      <ButtonSection
+        inputAdd={inputAdd}
+        clear={clear}
+        del={del}
+        plus={plus}
+        minus={minus}
+        divide={divide}
+        multiply= {multiply}
+        equals = {equals}
+      />
     </div>
   );
 }
---
+++ src/buttonSection.tsx
@@ -0,0 +1,173 @@
+import { FC } from "react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+import { RiDivideLine } from "react-icons/ri";
+import { FiDelete } from "react-icons/fi";
+
+interface IProps {
+  inputAdd: (num: string) => void;
+  clear: () => void;
+  del: () => void;
+  plus: () => void;
+  minus: () => void;
+  divide: () => void;
+  multiply: () => void;
+  equals: () => void;
+}
+
+const ButtonSection: FC<IProps> = ({
+  inputAdd,
+  clear,
+  del,
+  plus,
+  minus,
+  divide,
+  multiply,
+  equals
+}) => {
+  return (
+    <div className="grid grid-cols-4 grid-rows-5 gap-2 w-full">
+      <button
+        className=" text-3xl w-full bg-green-400 col-span-2 rounded-xl hover:bg-green-200"
+        onClick={() => {
+          clear();
+        }}
+      >
+        AC
+      </button>
+      <button
+        className="text-3xl grid place-items-center w-full bg-blue-400 rounded-xl hover:bg-blue-300"
+        onClick={() => {
+          del();
+        }}
+      >
+        <FiDelete />
+      </button>
+      <button
+        className=" py-3 text-3xl w-full grid place-items-center bg-blue-400 rounded-xl hover:bg-blue-300"
+        onClick={() => {
+          divide();
+        }}
+      >
+        <RiDivideLine />
+      </button>
+      <button
+        className="py-3 text-3xl bg-gray-300 hover:bg-gray-200 rounded-xl"
+        onClick={() => {
+          inputAdd("7");
+        }}
+      >
+        7
+      </button>
+      <button
+        className="py-3 text-3xl bg-gray-300 hover:bg-gray-200 rounded-xl"
+        onClick={() => {
+          inputAdd("8");
+        }}
+      >
+        8
+      </button>
+      <button
+        className="py-3 text-3xl bg-gray-300 hover:bg-gray-200 rounded-xl"
+        onClick={() => {
+          inputAdd("9");
+        }}
+      >
+        9
+      </button>
+      <button
+        className=" py-3 text-3xl w-full bg-blue-400 rounded-xl hover:bg-blue-300"
+        onClick={() => {
+          multiply();
+        }}
+      >
+        X
+      </button>
+      <button
+        className="py-3 text-3xl bg-gray-300 hover:bg-gray-200 rounded-xl"
+        onClick={() => {
+          inputAdd("4");
+        }}
+      >
+        4
+      </button>
+      <button
+        className="py-3 text-3xl bg-gray-300 hover:bg-gray-200 rounded-xl"
+        onClick={() => {
+          inputAdd("5");
+        }}
+      >
+        5
+      </button>
+      <button
+        className="py-3 text-3xl bg-gray-300 hover:bg-gray-200 rounded-xl"
+        onClick={() => {
+          inputAdd("6");
+        }}
+      >
+        6
+      </button>
+      <button
+        className=" py-3 text-3xl w-full bg-blue-400 grid place-items-center rounded-xl hover:bg-blue-300"
+        onClick={minus}
+      >
+        <AiOutlineMinus />
+      </button>
+      <button
+        className="py-3 text-3xl bg-gray-300 hover:bg-gray-200 rounded-xl"
+        onClick={() => {
+          inputAdd("1");
+        }}
+      >
+        1
+      </button>
+      <button
+        className="py-3 text-3xl bg-gray-300 hover:bg-gray-200 rounded-xl"
+        onClick={() => {
+          inputAdd("2");
+        }}
+      >
+        2
+      </button>
+      <button
+        className="py-3 text-3xl bg-gray-300 hover:bg-gray-200 rounded-xl"
+        onClick={() => {
+          inputAdd("3");
+        }}
+      >
+        3
+      </button>
+      <button
+        className=" py-3 grid place-items-center text-3xl w-full bg-blue-400 rounded-xl hover:bg-blue-300"
+        onClick={() => {
+          plus();
+        }}
+      >
+        <AiOutlinePlus />
+      </button>
+      <button
+        className="py-3 text-3xl bg-gray-300 hover:bg-gray-200 rounded-xl"
+        onClick={() => {
+          inputAdd("0");
+        }}
+      >
+        0
+      </button>
+      <button
+        className="py-3 text-3xl bg-gray-300 hover:bg-gray-200 rounded-xl"
+        onClick={() => {
+          inputAdd(".");
+        }}
+      >
+        .
+      </button>
+      <button
+        className="py-3 col-span-2 text-3xl bg-blue-300 hover:bg-blue-200 rounded-xl"
+        onClick={() => {equals()}}
+      >
+        =
+      </button>
+    </div>
+  );
+};
+
+export default ButtonSection;
